86c5d363a098e0ef4d8e64302d3059142ee593ae
mehakgmd <mehakgmd01@gmail.com>
2026-03-03 15:48:00 +0500

    error solved

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const serverClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2025-12-03",
   useCdn: false,
